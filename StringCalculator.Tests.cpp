d5ec85c5f34b1efaddc564b00b718b8ac92d1994
kalpanakammari <kalpana.kammari01@gmail.com>
2024-09-17 14:17:13 +0530

    Create StringCalculator.Tests.cpp

---
+++ StringCalculator.Tests.cpp
@@ -0,0 +1,12 @@
+#include"StringCalculator.h"
+#include<gtest/gtest.h>
+
+TEST(StringCalculatorTestSuite,add_emptyInputString_ZeroIsExpected){
+//Arrange
+  String input="";
+  int expectedValue=0;
+//Act
+  int actualValue=Add(input);
+//Assert
+  ASSERT_EQ(actualValue,expectedValue);
+}
