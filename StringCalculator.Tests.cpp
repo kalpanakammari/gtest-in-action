6b146ba08f394c5cb7abf07dd37b1ab478c346d2
kalpanakammari <kalpana.kammari01@gmail.com>
2024-09-17 15:00:28 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -45,4 +45,12 @@ int actualValue=Add(input);
 ASSERT_EQ(actualValue,expectedValue);
 }
 
+TEST(StringCalculatorTestSuite,when_passed_a_delimiter_SumIsExpected){
+ string input="//;\n2,4";
+int expectedValue=7;
+int actualValue=Add(input);
+ASSERT_EQ(actualValue,expectedValue);
+}
+
+
 
