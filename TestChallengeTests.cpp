5826279c0af4d28fc44ea99c63abbda92355fb9d
kalpanakammari <kalpana.kammari01@gmail.com>
2024-09-18 17:11:07 +0530

    Update TestChallengeTests.cpp

--- TestChallengeTests.cpp
+++ TestChallengeTests.cpp
@@ -1,1 +1,18 @@
+#include <getst/gtest.h>
+#include "TestChallenge.h"
+
+//Stub - Fake Dependency
+class NetworkAlerterStub:public INetworkAlerter{
+public:
+     int alert(float celcius) override{
+       return 500;
+            //
+     }
+};
+TEST(NetworkAlerterTestSuits,StateBasedTest){
+  NetworkAlerterStub stub;
+  int failureCount alertInCelciusFascade(400,&stub);
+  AssertEQ(failureCount,0);
+}
+
 
