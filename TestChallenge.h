98782bfcab20fc8b70164fd6e4a2c1752efdc0d6
kalpanakammari <kalpana.kammari01@gmail.com>
2024-09-18 16:52:32 +0530

    Update TestChallenge.h

--- TestChallenge.h
+++ TestChallenge.h
@@ -1,3 +1,11 @@
 // void alertInCelcius(float farenheit);
 // int getFailureCount();
-int alertInCelciusFacade(float farenheit);
+
+//Abstraction using class interface
+//functionpointer
+class INetworkAlerter{
+public:
+     virtual void alert(float celcius)=0; //virtual is used the function pointer which is created by compiler
+};
+
+int alertInCelciusFacade(float farenheit,INetworkAlerter *networkAlerterPtr);
