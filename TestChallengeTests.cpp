3cb4895d011772cebe7a108dc8b38532287e8328
kalpanakammari <kalpana.kammari01@gmail.com>
2024-09-18 17:40:46 +0530

    Update TestChallengeTests.cpp

--- TestChallengeTests.cpp
+++ TestChallengeTests.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 #include "TestChallenge.h"
+#include<gmock/gmock.h>
+using testing:FloatEq;
 
 
 //Stub - Fake Dependency
@@ -10,9 +12,24 @@ public:
             //
      }
 };
+//Mock implementation using Gmock
+class NetworkAlertMock:public INetworkAlerter{
+public:
+MOCK_METHOD(int alert,(float celcius));
+};
+
+
 TEST(NetworkAlerterTestSuits,StateBasedTest){
   NetworkAlerterStub stub;
   int failureCount= alertInCelciusFacade(400,&stub);
+  ASSERT_EQ(failureCount,1);
+}
+TEST(NetworkAlerterTestSuits,InteractionTest){
+  NetworkAlerterMock mock;
+     //EXCEPT_CALL (mockObject,method(Matcher).Times(Frequency).WillOnce/Repeat(Action)
+     
+     EXPECT_CALL(mock,alertEq(FloatEq(204,44))).Times(1).WillOnce(Return(500));
+  int failureCount= alertInCelciusFacade(400,&mock);
   ASSERT_EQ(failureCount,1);
 }
 
