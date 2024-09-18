96af1b3830e76e5355427fa3da24771cbbe04873
kalpanakammari <kalpana.kammari01@gmail.com>
2024-09-18 17:44:36 +0530

    Update TestChallengeTests.cpp

--- TestChallengeTests.cpp
+++ TestChallengeTests.cpp
@@ -1,37 +1,38 @@
 #include <gtest/gtest.h>
 #include "TestChallenge.h"
-#include<gmock/gmock.h>
-using testing::FloatEq;
-using testing::Return;
-
-
+#include <gmock/gmock.h>
+using testing::FloatEq;//matcher used to asset on method arguments
+using testing::Return; // Return value of mock function
 //Stub - Fake Dependency
 class NetworkAlerterStub:public INetworkAlerter{
 public:
-     int alert(float celcius) override{
-       return 500;
-            //
-     }
+      int  alert(float celcius) override{
+          return 500;
+      }
+
 };
-//Mock implementation using Gmock
-class NetworkAlertMock:public INetworkAlerter{
+
+//Mock Implementation using Gmock
+class NetworkAlerterMock:public INetworkAlerter{
 public:
-MOCK_METHOD(int alert,(float celcius));
+             MOCK_METHOD(int ,alert,(float celcius));
 };
 
-
-TEST(NetworkAlerterTestSuits,StateBasedTest){
+TEST(NetworkAlerterTestSuite,StateBasedTest){
   NetworkAlerterStub stub;
-  int failureCount= alertInCelciusFacade(400,&stub);
-  ASSERT_EQ(failureCount,1);
+    int failureCount=  alertInCelciusFacade(400,&stub);
+    ASSERT_EQ(failureCount,1);
 }
-TEST(NetworkAlerterTestSuits,InteractionTest){
-  NetworkAlerterMock mock;
-     //EXCEPT_CALL (mockObject,method(Matcher).Times(Frequency).WillOnce/Repeat(Action)
-     
-     EXPECT_CALL(mock,alertEq(FloatEq(204,44))).Times(1).WillOnce(Return(500));
-  int failureCount= alertInCelciusFacade(400,&mock);
-  ASSERT_EQ(failureCount,1);
+TEST(NetworkAlerterTestSuite,InteractionTest){
+    NetworkAlerterMock mock;
+      // EXPECT_CALL (mockObject,method(Matcher).Times(Frequency).WillOnce/Repeat(Action)
+      
+  EXPECT_CALL(mock,alert(FloatEq(204.44))).Times(1).WillOnce(Return(500));
+    int failureCount=  alertInCelciusFacade(400,&mock);
+    ASSERT_EQ(failureCount,1);
+    
 }
+
+
 
 
